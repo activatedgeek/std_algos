ed46a47db5b310b0d0fe01ea16603a51bf4e37b9
Sanyam Kapoor <ksanyam18@yahoo.com>
2013-12-28 00:40:40 +0530

    Create sport_template.cpp
    Template for programming competitions

---
+++ sport_template.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <sstream>
+#include <math.h>
+#include <vector>
+#include <utility>
+#include <list>
+#include <stack>
+#include <queue>
+using namespace std;
+
+#define LIM INT_MAX
+
+//iterations
+#define repi(i,a,b) for(int i=a;i<=b;++i)
+#define repd(i,a,b) for(int i=a;i>=b;--i)
+
+//utility
+#define reset(arr,val,type,size) memset(arr,val,sizeof(type)*size)
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		
+	}
+	return 0;
+}
