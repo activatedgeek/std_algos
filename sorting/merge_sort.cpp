21c41e942c692a06301574d80c3f43ba0740a417
activatedgeek <ksanyam18@yahoo.com>
2014-01-16 18:40:35 +0530

    updated wierd errors

--- sorting/merge_sort.cpp
+++ sorting/merge_sort.cpp
@@ -1,72 +1,52 @@
-/**MERGE SORT**/
-
-#include<iostream>
-#include<fstream>
-#include<cstdlib>
-#include<sstream>   //to directly convert int to string
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <sstream> 
 using namespace std;
 
-void mergesorted(int *arr,int count,int start,int mid,int end){   //Sub-routine to merge two sorted arrays
-	int a[mid-start],b[end-mid],i=0;
-	while(i<mid-start)
-		a[i++]=arr[start+i];
-	i=0;
-	while(i<end-mid)
-		b[i++] = arr[mid+i];
-		
-	int left=0,right=0;
-	i=start;
-	while(left<mid-start && right<end-mid){
-		if(a[left]<b[right]){
-			arr[i++]=a[left++];
-		}
+long long mergesorted(long long *arr,long long n,long long low,long long mid,long long high){
+	long long i=low,j=mid,k=low;
+	long long temp[n],count=0;
+	while(i<mid && j<=high){
+		if(arr[i]<=arr[j])
+			temp[k++]=arr[i++];
 		else{
-			arr[i++]=b[right++];
+			temp[k++]=arr[j++];
+			count+= (mid-i);
 		}
 	}
-	if(left<mid-start){
-		while(left<mid-start)
-			arr[i++]=a[left++];
-	}
-	else if(right<end-mid){
-		while(right<end-mid)
-			arr[i++]=b[right++];
-	}
+
+	while(i<mid)
+		temp[k++]=arr[i++];
+	while(j<=high)
+		temp[k++]=arr[j++];
+	for(i=low;i<=high;++i)
+		arr[i]=temp[i];
+	return count;
 }
 
-void mergesort(int *arr,int count,int low,int high){  //Main routine to divide the problem into two halves in every recursive step
-	if(low<high-1){
-		int mid = (low+high)/2;
-		mergesort(arr,count,low,mid);
-		mergesort(arr,count,mid,high);
-		mergesorted(arr,count,low,mid,high);
+long long mergesort(long long *arr,long long n,long long low,long long high){
+	long long count=0;
+	if(low<high){
+		long long mid = (low+high)/2;
+		count+=mergesort(arr,n,low,mid);
+		count+=mergesort(arr,n,mid+1,high);
+		count+=mergesorted(arr,n,low,mid+1,high);
 	}
 }
 
 int main(){
-	fstream input;
-	int count,i;
-	
-	//Filestream to take input from the file
-	input.open("input.txt",ios::in);
-	if(!input.good()){
-		cout<<"Could not open\n";
-		return 1;
+	long long t;
+	cin>>t;
+	while(t--){
+		long long n;
+		cin>>n;
+		long long arr[n];
+		for(long long i=0;i<n;++i)
+			cin>>arr[i];
+
+		long long res = mergesort(arr,n,0,n-1);
+		cout<<res+1<<"\n";
 	}
-	input>>count;
-	int *arr = new int[count]; //Dyamically allocated memory for the array
-	int s;
-	i=0;
-	while(!input.eof()){
-		input>>s;
-		arr[i] = s;
-		i++;
-	}
-	input.close();
-	
-	mergesort(arr,count,0,count); //Call to the mergesort sort routine
-	
-	delete arr;  //deleting the allocated memory for array to prevent memory leaks
 	return 0;
 }
-
