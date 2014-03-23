d78a696b038e47c0fae826766b11ef36081ac712
activatedgeek <ksanyam18@yahoo.com>
2014-03-23 16:41:14 +0530

    binarsady indexed tree DS for 1D and 2

---
+++ utils/BIT.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int read(int* tree,int index){
+	int sum=0;
+	while(index){
+		sum += tree[index];
+		index -= (index & -index);
+	}
+	return sum;
+}
+
+void update(int* tree,int change,int index,int n){
+	while(index<=n){
+		tree[index]+=change;
+		index+= (index & -index);
+	}
+}
+
+void initTree(int *tree,int* cumu,int n){
+	memset(tree,0,sizeof(int)*(n+1));
+	for(int i=1;i<=n;++i){
+		update(tree,cumu[i],i,n);
+	}
+}
+/*
+int read2D(int sx,int sy,int ex,int ey){
+	int sum=0;
+	sum += read(sx,sy);
+	if(ex>1)
+		sum -= read(ex-1,sy);
+	if(ey>1)
+		sum -= read(sx,ey-1);
+	if(ex>1 && ey>1){
+		sum += read(ex-1,ey-1);
+	}
+	return sum;
+}*/
+
+
+int main(){
+	int n;
+	cin>>n;
+	int arr[n+1],tree[n+1];
+	arr[0]=0;
+	for(int i=1;i<=n;++i)
+		cin>>arr[i];
+	initTree(tree,arr,n);
+
+	for(int i=1;i<=n;++i){
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+
+	cout<<read(tree,8)-read(tree,7)<<endl;
+
+	return 0;
+}
